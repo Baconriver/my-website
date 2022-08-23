c9d6cc73060adca037777cc5611d3ba9e9c52879
Baconriver <riverjiangjh@gmail.com>
2022-08-23 23:49:08 +0930

    inital website

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Projects from './components/Projects';
 import Experience from './components/Experience';
 import Social from './components/Social';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<div className="App">
+			<Navbar />
 			<Header />
 			<About />
 			<Projects />
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import profile from '../assets/profile.png';
 const Header = () => {
     return (
         <section className='header'>
-            <img src={profile} alt="John Doe" />
+            {/* <img src={profile} alt="Henry Jiang" /> */}
 
             <div className='header__content'>
-                <h1>Hi, I'm John Doe</h1>
+                <h1>Hi, I'm Henry Jiang</h1>
                 <p>Blockchain Developer</p>
                 <button className='button'>Get In Touch</button>
             </div>
