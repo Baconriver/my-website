e1ea4f2df9fc39b1b6f32d8a8f96ca6d8836ed9b
Baconriver <henryjiangjh@gmail.com>
2022-11-10 20:29:43 +1030

    contact ui

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -28,34 +28,45 @@ const Contact = () => {
     <section className="contact" id="contact">
       <h2>Contact me</h2>
       <form ref={form} onSubmit={handleSubmit(sendEmail)}>
-        <input
-          id="contact-name"
-          type="text"
-          name="name"
-          placeholder="Name"
-          {...register("name", {
-            required: true,
-            maxLength: 20,
-            pattern: /^[A-Za-z ]+$/i,
-          })}
-        />
-        {errors?.name?.type === "required" && <p>Please enter your name</p>}
-        {errors?.name?.type === "maxLength" && (
-          <p>Name cannot exceed 20 characters</p>
-        )}
-        {errors?.name?.type === "pattern" && (
-          <p>Alphabetical characters only</p>
-        )}
-        <input
-          placeholder="Mobile"
-          id="contact-mobile"
-          type="text"
-          name="mobile"
-          {...register("mobile", {
-            pattern: /^[0-9]+$/i,
-          })}
-        />
-        {errors?.mobile?.type === "pattern" && <p>Numberic only </p>}
+        <div className="contact-first-row">
+          <div className="contact-name">
+            <input
+              id="contact-name"
+              type="text"
+              name="name"
+              placeholder="Name"
+              {...register("name", {
+                required: true,
+                maxLength: 20,
+                pattern: /^[A-Za-z ]+$/i,
+              })}
+            />
+            {errors?.name?.type === "required" && (
+              <p className="error-message">Please enter your name</p>
+            )}
+            {errors?.name?.type === "maxLength" && (
+              <p className="error-message">Name cannot exceed 20 characters</p>
+            )}
+            {errors?.name?.type === "pattern" && (
+              <p className="error-message">Alphabetical characters only</p>
+            )}
+          </div>
+          <div className="contact-mobile">
+            <input
+              placeholder="Mobile"
+              id="contact-mobile"
+              type="text"
+              name="mobile"
+              {...register("mobile", {
+                pattern: /^[0-9]+$/i,
+              })}
+            />
+            {errors?.mobile?.type === "pattern" && (
+              <p className="error-message">Numberic only </p>
+            )}
+          </div>
+        </div>
+
         <input
           placeholder="Email"
           id="contact-email"
@@ -66,8 +77,12 @@ const Contact = () => {
             pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
           })}
         />
-        {errors?.email?.type === "required" && <p>Please enter your email</p>}
-        {errors?.email?.type === "pattern" && <p>Wrong Email pattern</p>}
+        {errors?.email?.type === "required" && (
+          <p className="error-message">Please enter your email</p>
+        )}
+        {errors?.email?.type === "pattern" && (
+          <p className="error-message">Wrong Email pattern</p>
+        )}
         <textarea
           id="contact-content"
           name="message"
@@ -77,9 +92,15 @@ const Contact = () => {
             maxLength: 2000,
           })}
         />
-        {errors?.name?.type === "required" && <p>Please enter your message</p>}
-        {errors?.name?.type === "maxLength" && <p>Message is too long</p>}
-        <input type="submit" />
+        {errors?.message?.type === "required" && (
+          <p className="error-message">Please enter your message</p>
+        )}
+        {errors?.message?.type === "maxLength" && (
+          <p className="error-message">Message is too long</p>
+        )}
+        <button className="contact-submit" type="submit">
+          Submit
+        </button>
       </form>
     </section>
   );
