ab2961a7a7d189318b9ec10eb01b48b42c4e504d
Baconriver <henryjiangjh@gmail.com>
2023-04-01 22:26:02 +1030

    resume

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -1,22 +1,19 @@
 import "./Social.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faFileLines } from "@fortawesome/free-solid-svg-icons";
+import { faFileLines } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 const Social = () => {
   return (
     <div className="social" id="socials">
       <h2>Socials</h2>
 
-      <a href="" className="button">
-        <FontAwesomeIcon icon={faEnvelope} />
-      </a>
       <a
         href="https://www.linkedin.com/in/hejianghenry/"
         className="button"
         target="_blank"
         rel="noreferrer"
       >
-        <FontAwesomeIcon icon={faLinkedin} />
+        <FontAwesomeIcon icon={faLinkedin} /> Linkedin
       </a>
       <a
         href="https://github.com/Baconriver"
@@ -24,10 +21,14 @@ const Social = () => {
         target="_blank"
         rel="noreferrer"
       >
-        <FontAwesomeIcon icon={faGithub} />
+        <FontAwesomeIcon icon={faGithub} /> Github
       </a>
-      <a href="" className="button">
-        <FontAwesomeIcon icon={faFileLines} />
+      <a
+        href="/src/assets/Henry_Jiang.pdf"
+        download="Henry_Jiang.pdf"
+        className="button"
+      >
+        <FontAwesomeIcon icon={faFileLines} /> Resume
       </a>
     </div>
   );
