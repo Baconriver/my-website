7b52a53fb0f6b1bb7d995e5ace4a9b5c5197486e
Baconriver <henryjiangjh@gmail.com>
2023-04-20 23:57:30 +0930

    minor fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -30,91 +30,95 @@ const Contact = () => {
   return (
     <section className="contact" id="contact">
       <h2>Contact me</h2>
-      <form ref={form} onSubmit={handleSubmit(sendEmail)}>
-        <div className="contact-first-row">
-          <div className="contact-name">
-            <input
-              id="contact-name"
-              type="text"
-              name="name"
-              placeholder="Name"
-              className={errors?.name ? "invalid" : ""}
-              {...register("name", {
-                required: true,
-                maxLength: 20,
-                pattern: /^[A-Za-z ]+$/i,
-              })}
-            />
-            {errors?.name?.type === "required" && (
-              <p className="error-message">Please enter your name</p>
-            )}
-            {errors?.name?.type === "maxLength" && (
-              <p className="error-message">Name cannot exceed 20 characters</p>
-            )}
-            {errors?.name?.type === "pattern" && (
-              <p className="error-message">Alphabetical characters only</p>
-            )}
+      <div className="contact-container">
+        <form ref={form} onSubmit={handleSubmit(sendEmail)}>
+          <div className="contact-first-row">
+            <div className="contact-name">
+              <input
+                id="contact-name"
+                type="text"
+                name="name"
+                placeholder="Name"
+                className={errors?.name ? "invalid" : ""}
+                {...register("name", {
+                  required: true,
+                  maxLength: 20,
+                  pattern: /^[A-Za-z ]+$/i,
+                })}
+              />
+              {errors?.name?.type === "required" && (
+                <p className="error-message">Please enter your name</p>
+              )}
+              {errors?.name?.type === "maxLength" && (
+                <p className="error-message">
+                  Name cannot exceed 20 characters
+                </p>
+              )}
+              {errors?.name?.type === "pattern" && (
+                <p className="error-message">Alphabetical characters only</p>
+              )}
+            </div>
+            <div className="contact-mobile">
+              <input
+                placeholder="Mobile"
+                id="contact-mobile"
+                type="text"
+                name="mobile"
+                className={errors?.mobile ? "invalid" : ""}
+                {...register("mobile", {
+                  pattern: /^[0-9]+$/i,
+                })}
+              />
+              {errors?.mobile?.type === "pattern" && (
+                <p className="error-message">Numberic only </p>
+              )}
+            </div>
           </div>
-          <div className="contact-mobile">
-            <input
-              placeholder="Mobile"
-              id="contact-mobile"
-              type="text"
-              name="mobile"
-              className={errors?.mobile ? "invalid" : ""}
-              {...register("mobile", {
-                pattern: /^[0-9]+$/i,
-              })}
-            />
-            {errors?.mobile?.type === "pattern" && (
-              <p className="error-message">Numberic only </p>
-            )}
-          </div>
-        </div>
 
-        <input
-          placeholder="Email"
-          id="contact-email"
-          type="email"
-          name="email"
-          className={errors?.email ? "invalid" : ""}
-          {...register("email", {
-            required: true,
-            pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-          })}
-        />
-        {errors?.email?.type === "required" && (
-          <p className="error-message">Please enter your email</p>
-        )}
-        {errors?.email?.type === "pattern" && (
-          <p className="error-message">Wrong Email pattern</p>
-        )}
-        <textarea
-          id="contact-content"
-          name="message"
-          placeholder="Something to say..."
-          className={errors?.message ? "invalid" : ""}
-          {...register("message", {
-            required: true,
-            maxLength: 2000,
-          })}
-        />
-        {errors?.message?.type === "required" && (
-          <p className="error-message">Please enter your message</p>
-        )}
-        {errors?.message?.type === "maxLength" && (
-          <p className="error-message">Message is too long</p>
-        )}
-        <motion.button
-          whileHover={{ scale: 1.05 }}
-          transition={{ type: "spring", stiffness: 400, damping: 10 }}
-          className="contact-submit"
-          type="submit"
-        >
-          Send
-        </motion.button>
-        <ToastContainer position="top-right" closeOnClick theme="colored" />
-      </form>
+          <input
+            placeholder="Email"
+            id="contact-email"
+            type="email"
+            name="email"
+            className={errors?.email ? "invalid" : ""}
+            {...register("email", {
+              required: true,
+              pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+            })}
+          />
+          {errors?.email?.type === "required" && (
+            <p className="error-message">Please enter your email</p>
+          )}
+          {errors?.email?.type === "pattern" && (
+            <p className="error-message">Wrong Email pattern</p>
+          )}
+          <textarea
+            id="contact-content"
+            name="message"
+            placeholder="Something to say..."
+            className={errors?.message ? "invalid" : ""}
+            {...register("message", {
+              required: true,
+              maxLength: 2000,
+            })}
+          />
+          {errors?.message?.type === "required" && (
+            <p className="error-message">Please enter your message</p>
+          )}
+          {errors?.message?.type === "maxLength" && (
+            <p className="error-message">Message is too long</p>
+          )}
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+            className="contact-submit"
+            type="submit"
+          >
+            Send
+          </motion.button>
+          <ToastContainer position="top-right" closeOnClick theme="colored" />
+        </form>
+      </div>
     </section>
   );
 };
